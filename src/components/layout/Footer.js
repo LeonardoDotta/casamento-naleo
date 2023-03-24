0428f6db0855b9ddc4568020e51c6c537e230313
LeonardoDotta <ldotta918@gmail.com>
2023-03-24 18:21:10 -0300

    atualizações

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,4 @@
-import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
 
 import styles from './Footer.module.css'
 
@@ -6,14 +6,20 @@ function Footer() {
     return (
     <footer className={styles.footer}>
         <ul className={styles.social_list}>
-            <li><FaFacebook /></li>
-            <a href='https://www.instagram.com/leo_dotta26'
+            <a className={styles.social_list_instagram2} href='https://www.instagram.com/nayaramelom'
             target="_blank"
             body="hello world"
             rel="noopener noreferrer">
             <li><FaInstagram /></li>
+            <p>@nayaramelom</p>
             </a>
-            <li><FaLinkedin /></li>
+            <a className={styles.social_list_instagram1} href='https://www.instagram.com/leo_dotta26'
+            target="_blank"
+            body="hello world"
+            rel="noopener noreferrer">
+            <li><FaInstagram /></li>
+            <p>@leo_dotta26</p>
+            </a>
         </ul>
         <p className={styles.copy_right}><span>NALEO</span> &copy; 2023</p>
     </footer>
--- src/components/pages/Presentes/Produtos.js
+++ src/components/pages/Presentes/Produtos.js
@@ -1,9 +1,9 @@
 import './Produtos.css'
-// import { Button, Space } from 'antd'
-// import { useState } from 'react'
+import { Button, Space } from 'antd'
+import { useState } from 'react'
 
 function Produtos({nome, descricao, preco, parcelas, preço2, id, link}) {
-    // const[interruptor, setInterruptor]=useState("off")
+     const[interruptor, setInterruptor]=useState("off")
     return (
         <div className="produtos">
             <div className="produtos-image" id={id}></div>
@@ -11,7 +11,7 @@ function Produtos({nome, descricao, preco, parcelas, preço2, id, link}) {
             <p>{descricao}</p>
             <p>R$ <span className="bold">{preco}</span> ou em até <span className="bold">{parcelas}</span> de R$ <span className="bold">{preço2}</span></p>
             <a href={link} className="btn">Comprar</a>
-            {/* {interruptor=="on"?"pai ta on":""}
+            {interruptor=="on"?"pai ta on":""}
             <Button 
                 onClick={()=>{
                     if (interruptor=="off"){
@@ -22,7 +22,7 @@ function Produtos({nome, descricao, preco, parcelas, preço2, id, link}) {
                     }
                 }}>
                     Default Button
-            </Button> */}
+            </Button>
         </div>
     )
 }
