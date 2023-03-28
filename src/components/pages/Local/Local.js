bfd377a4bd99a38e8adee48ee71889e34bc7f4e1
LeonardoDotta <ldotta918@gmail.com>
2023-03-28 17:56:40 -0300

    local feito

--- src/components/pages/Local/Local.js
+++ src/components/pages/Local/Local.js
@@ -2,12 +2,16 @@ import styles from './Local.module.css'
 
 function Local() {
     return (
-    <h1>
-        Local
-        <iframe className={styles.map}
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3700.10533462283!2d-47.90023608504131!3d-21.968920010599895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b8705c432de473%3A0x98770a3c9bd378c3!2sAv.%20Miguel%20Damha%2C%201400%20-%20Res.%20Damha%20II%2C%20S%C3%A3o%20Carlos%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1679681179099!5m2!1spt-BR!2sbr"> 
-        </iframe>
-    </h1>
+    <div className={styles.div}>
+        <h1 className={styles.local}>
+         Local
+        </h1>
+         <div>
+            <iframe className={styles.map}
+             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3700.10533462283!2d-47.90023608504131!3d-21.968920010599895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b8705c432de473%3A0x98770a3c9bd378c3!2sAv.%20Miguel%20Damha%2C%201400%20-%20Res.%20Damha%20II%2C%20S%C3%A3o%20Carlos%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1679681179099!5m2!1spt-BR!2sbr"> 
+             </iframe>
+        </div>
+    </div>
     )
 }
 
