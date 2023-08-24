ad0a320928d7792fd6193b5f5706853789d60e78
LeonardoDotta <ldotta918@gmail.com>
2023-08-24 17:16:24 -0300

    finalização pro portfolio

--- src/components/pages/Local/Local.js
+++ src/components/pages/Local/Local.js
@@ -8,7 +8,7 @@ function Local() {
                 Local
             </div>
             <iframe className='map'
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3700.10533462283!2d-47.90023608504131!3d-21.968920010599895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b8705c432de473%3A0x98770a3c9bd378c3!2sAv.%20Miguel%20Damha%2C%201400%20-%20Res.%20Damha%20II%2C%20S%C3%A3o%20Carlos%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1679681179099!5m2!1spt-BR!2sbr">
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.1974904582185!2d-46.65906922386872!3d-23.56134956159329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8da0aa315%3A0xd59f9431f2c9776a!2sAv.%20Paulista%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1692908032081!5m2!1spt-BR!2sbr">
             </iframe>
         </div>
     )
